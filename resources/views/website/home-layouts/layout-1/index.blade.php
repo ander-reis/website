11e72966fa70116cb9080ae0385768c148e955a3
Anderson <anderson@sinprosp.org.br>
2019-02-15 16:39:07 -0200

    Commit Teste

--- resources/views/website/home-layouts/layout-1/index.blade.php
+++ resources/views/website/home-layouts/layout-1/index.blade.php
@@ -5,8 +5,4 @@
     @component('website.home-layouts.layout-1.components.intro')@endcomponent
     @component('website.home-layouts.layout-1.components.noticias')@endcomponent
 {{--    @component('website.components.layout-1._slider', ['sliders' => $sliders])@endcomponent--}}
-
-
-
-
 @endsection
