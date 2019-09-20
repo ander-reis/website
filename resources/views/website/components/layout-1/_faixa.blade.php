195404e0f58052ab70eea18e31c4aa20b15d7b20
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-09-20 15:19:48 -0300

    colocado link na faixa

--- resources/views/website/components/layout-1/_faixa.blade.php
+++ resources/views/website/components/layout-1/_faixa.blade.php
@@ -2,19 +2,19 @@
     <div class="owl-carousel owl-theme pt-3">
         <div class="item text-center">
             <i class="fa fa-arrow-right" aria-hidden="true"></i>
-            <a href="#" class="black faixa_size">Cursos e <br>congressos</a>
+            <a href="http://sinprosp.org.br/escola.asp" class="black faixa_size">Cursos e <br>congressos</a>
         </div>
         <div class="item text-center">
             <i class="fa fa-arrow-right" aria-hidden="true"></i>
-            <a href="#" class="black faixa_size">Salários e<br/>reajustes</a>
+            <a href="http://sinprosp.org.br/guia_consultas.asp#salario" class="black faixa_size">Salários e<br/>reajustes</a>
         </div>
         <div class="item text-center">
             <i class="fa fa-arrow-right" aria-hidden="true"></i>
-            <a href="#" class="black faixa_size">Convenções e<br/>acordos</a>
+            <a href="http://sinprosp.org.br/convencoes_acordos.asp" class="black faixa_size">Convenções e<br/>acordos</a>
         </div>
         <div class="item text-center">
             <i class="fa fa-arrow-right" aria-hidden="true"></i>
-            <a href="#" class="black faixa_size">Guia de<br/>direitos</a>
+            <a href="http://sinprosp.org.br/guia_consultas.asp" class="black faixa_size">Guia de<br/>direitos</a>
         </div>
     </div>
     {{-- <div class="owl-theme">
