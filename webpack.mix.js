72d841875cb8bd4777fb8dbe9fe7a4dd23bee45b
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-09-30 12:25:59 -0300

    reduzido tamanho da pasta public/fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/fonts')
+    //.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/fonts')
     .sourceMaps();
 
 mix.browserSync('website.br');
