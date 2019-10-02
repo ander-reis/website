3a3740506cb471304d7061162c33c434ba36f00e
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-10-02 12:23:48 -0300

    correcao paginas principais

--- resources/views/website/paginas-principais/show.blade.php
+++ resources/views/website/paginas-principais/show.blade.php
@@ -6,8 +6,8 @@
 
     <div class="row">
         <div class="col-md-9 mb-3 mb-md-5">
-            <h2>{!! $pagina->ds_texto !!}</h2>
-            <div class="mt-4">{!! $pagina->txt_pagina !!}</div>
+            <h2>{!! $pagina->txt_titulo !!}</h2>
+            <div class="mt-4">{!! $pagina->ds_texto !!}</div>
         </div>
         @component('website.components.layout-1._column_right')@endcomponent
     </div>
