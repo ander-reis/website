56937b4fe05a4b4cf6ae57c61741d4943b24026d
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-10-02 12:09:42 -0300

    img intro desktop / mobile

--- resources/views/website/components/layout-1/_intro.blade.php
+++ resources/views/website/components/layout-1/_intro.blade.php
@@ -1,7 +1,8 @@
 <div>
     <a href={!! $intro[0]->ds_link !!}>
         {{-- <img class="img-fluid rounded" src="{{ asset('images/layout-1/intro/img_intro.jpg') }}" alt="{!! $intro[0]->ds_titulo !!}"> --}}
-        <img class="img-fluid rounded intro-img" src="{{ asset('/storage/intro/' . $intro[0]->id . '/' . $intro[0]->ds_imagem_desktop) }}" alt="{!! $intro[0]->ds_titulo !!}">
+        <img class="img-fluid rounded intro-img d-none d-sm-block d-lg-block" src="{{ asset('/storage/intro/' . $intro[0]->id . '/' . $intro[0]->ds_imagem_desktop) }}" alt="{!! $intro[0]->ds_titulo !!}">
+        <img class="img-fluid rounded intro-img d-block d-sm-none d-lg-none" src="{{ asset('/storage/intro/' . $intro[0]->id . '/' . $intro[0]->ds_imagem_mobile) }}" alt="{!! $intro[0]->ds_titulo !!}">
     </a>
 </div>
 <div class="line-section intro_border"></div>
