197e346551e6e6da927a1a0bfeac57db37d6de31
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-10-02 15:28:58 -0300

    corrigido link servicos

--- resources/views/website/components/layout-1/_servicos.blade.php
+++ resources/views/website/components/layout-1/_servicos.blade.php
@@ -6,28 +6,32 @@
                 'route' => route('paginas-principais', ['url-pagina' => 'assistencia-juridica']),
                 'icon' => '<i class="fas fa-balance-scale fa-3x text-dark"></i>',
                 'class' => 'servicos1',
-                'title' => 'ATENDIMENTO<br/>JURÍDICO'
+                'title' => 'ATENDIMENTO<br/>JURÍDICO',
+                'target' => ""
             ])
 
             @include('website.components.layout-1._icons_servicos', [
                 'route' => route('paginas-principais', ['url-pagina' => 'assistencia-previdenciaria']),
                 'icon' => '<i class="fas fa-tasks fa-3x text-dark"></i>',
                 'class' => 'servicos1',
-                'title' => 'ATENDIMENTO<br/>PREVIDENCIÁRIO'
+                'title' => 'ATENDIMENTO<br/>PREVIDENCIÁRIO',
+                'target' => ""
             ])
 
             @include('website.components.layout-1._icons_servicos', [
                 'route' => route('fono.index'),
                 'icon' => '<i class="fab fa-creative-commons-sampling fa-3x text-dark"></i>',
                 'class' => 'servicos1',
-                'title' => 'FONOAUDIOLOGIA'
+                'title' => 'FONOAUDIOLOGIA',
+                'target' => ""
             ])
 
             @include('website.components.layout-1._icons_servicos', [
                 'route' => route('paginas-principais', ['url-pagina' => 'colonia']),
                 'icon' => '<i class="fas fa-umbrella-beach fa-3x text-dark"></i>',
                 'class' => 'servicos1',
-                'title' => 'COLÔNIA<br/>DE FÉRIAS'
+                'title' => 'COLÔNIA<br/>DE FÉRIAS',
+                'target' => ""
             ])
         </div>
     </div>
@@ -36,31 +40,35 @@
         <div class="row">
 
            @include('website.components.layout-1._icons_servicos', [
-                'route' => route('fono.index'),
+                'route' => 'http://planoprofessor.com.br/',
                 'icon' => '<i class="fas fa-notes-medical fa-3x text-dark"></i>',
                 'class' => 'servicos2',
-                'title' => 'PLANO DE SAÚDE'
+                'title' => 'PLANO DE SAÚDE',
+                'target' => "_blank"
             ])
 
             @include('website.components.layout-1._icons_servicos', [
-                'route' => route('fono.index'),
+                'route' => 'http://www.sinprosp.org.br/convenios.asp',
                 'icon' => '<i class="fas fa-handshake fa-3x text-dark"></i>',
                 'class' => 'servicos2',
-                'title' => 'CONVÊNIOS<br/>E PARCERIAS'
+                'title' => 'CONVÊNIOS<br/>E PARCERIAS',
+                'target' => "_blank"
             ])
 
             @include('website.components.layout-1._icons_servicos', [
-                'route' => route('fono.index'),
+                'route' => route('paginas-principais', ['url-pagina' => 'homologacao']),
                 'icon' => '<i class="fas fa-copy fa-3x text-dark"></i>',
                 'class' => 'servicos2',
-                'title' => 'HOMOLOGAÇÃO'
+                'title' => 'HOMOLOGAÇÃO',
+                'target' => ""
             ])
 
             @include('website.components.layout-1._icons_servicos', [
-                'route' => route('fono.index'),
+                'route' => 'https://www.youtube.com/user/SINPROSP',
                 'icon' => '<i class="fas fa-folder-plus fa-3x text-dark"></i>',
                 'class' => 'servicos2',
-                'title' => 'ACERVO'
+                'title' => 'ACERVO',
+                'target' => "_blank"
             ])
         </div>
     </div>
@@ -72,28 +80,32 @@
                 'route' => route('atendimento-eletronico.index'),
                 'icon' => '<i class="fas fa-comments fa-3x text-dark"></i>',
                 'class' => 'servicos3',
-                'title' => 'ATENDIMENTO<br/>ELETRÔNICO'
+                'title' => 'ATENDIMENTO<br/>ELETRÔNICO',
+                'target' => ""
             ])
 
             @include('website.components.layout-1._icons_servicos', [
                 'route' => route('boletim.index'),
                 'icon' => '<i class="fas fa-envelope-open-text fa-3x text-dark"></i>',
                 'class' => 'servicos3',
-                'title' => 'BOLETIM<br/>DO SINPROSP'
+                'title' => 'BOLETIM<br/>DO SINPROSP',
+                'target' => ""
             ])
 
             @include('website.components.layout-1._icons_servicos', [
-                'route' => route('fono.index'),
+                'route' => route('paginas-principais', ['url-pagina' => 'app']),
                 'icon' => '<i class="fas fa-mobile-alt fa-3x text-dark"></i>',
                 'class' => 'servicos3',
-                'title' => 'APLICATIVO<br/>DO SINPROSP'
+                'title' => 'APLICATIVO<br/>DO SINPROSP',
+                'target' => ""
             ])
 
             @include('website.components.layout-1._icons_servicos', [
                 'route' => route('paginas-principais', ['url-pagina' => 'whatsapp']),
                 'icon' => '<i class="fab fa-whatsapp fa-3x text-dark"></i>',
                 'class' => 'servicos3',
-                'title' => 'WHATSAPP'
+                'title' => 'WHATSAPP',
+                'target' => ""
             ])
         </div>
     </div>
