da62e04ab03d822059ca95fd3edfbbb4ce0db526
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-10-04 14:44:46 -0300

    links para o www1

--- resources/views/website/components/layout-1/_column_right.blade.php
+++ resources/views/website/components/layout-1/_column_right.blade.php
@@ -1,7 +1,7 @@
 <div class="col-12 col-md-3 d-none d-sm-block border-left border-secondary">
     <div class="row">
         <div class="col">
-                <a href="http://www.sinprosp.org.br/sindicalizacao.asp" target="_blank">
+                <a href="http://www1.sinprosp.org.br/sindicalizacao.asp" target="_blank">
                     <img class="img-fluid rounded"
                         src="{{ asset('images/layout-1/images-coluna/sindicalizese.png') }}" alt="SinproSP">
                 </a>
@@ -23,7 +23,7 @@
     </div>
     <div class="row mt-2">
         <div class="col">
-                <a href="http://www.sinprosp.org.br/cadastro_login.asp" target="_blank">
+                <a href="http://www1.sinprosp.org.br/cadastro_login.asp" target="_blank">
                     <img class="mg-fluid rounded"
                         src="{{ asset('images/layout-1/images-coluna/cadastro.png') }}" alt="SinproSP">
                 </a>
