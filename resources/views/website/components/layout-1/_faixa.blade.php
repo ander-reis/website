c6a84da21c5c15c3799b4dadfee146d926c52e35
LEANDRO CAPARROZ <leandrocaparroz@yahoo.com.br>
2019-10-04 10:30:10 -0300

    Acerto de links

--- resources/views/website/components/layout-1/_faixa.blade.php
+++ resources/views/website/components/layout-1/_faixa.blade.php
@@ -2,11 +2,11 @@
     <div class="owl-carousel owl-theme pt-3">
         <div class="item text-center">
             <i class="fa fa-arrow-right" aria-hidden="true"></i>
-            <a href="http://sinprosp.org.br/escola.asp" class="black faixa_size" target="_blank">Cursos e <br>congressos</a>
+            <a href="http://www1.sinprosp.org.br/escola.asp" class="black faixa_size" target="_blank">Cursos e <br>congressos</a>
         </div>
         <div class="item text-center">
             <i class="fa fa-arrow-right" aria-hidden="true"></i>
-            <a href="http://sinprosp.org.br/guia_consultas.asp#salario" class="black faixa_size" target="_blank">Salários e<br/>reajustes</a>
+            <a href="http://www1.sinprosp.org.br/guia_consultas.asp#salario" class="black faixa_size" target="_blank">Salários e<br/>reajustes</a>
         </div>
         <div class="item text-center">
             <i class="fa fa-arrow-right" aria-hidden="true"></i>
@@ -14,7 +14,7 @@
         </div>
         <div class="item text-center">
             <i class="fa fa-arrow-right" aria-hidden="true"></i>
-            <a href="http://sinprosp.org.br/guia_consultas.asp" class="black faixa_size" target="_blank">Guia de<br/>direitos</a>
+            <a href="http://www1.sinprosp.org.br/guia_consultas.asp" class="black faixa_size" target="_blank">Guia de<br/>direitos</a>
         </div>
     </div>
     {{-- <div class="owl-theme">
