5e45fc4afbce02142a19b199cc44e239fc6ceb55
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-10-07 06:30:29 -0300

    incluido privacidade redirect noticias.asp

---
+++ public/noticias.asp/index.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: ' . 'http://www1.sinprosp.org.br/noticias.asp?' . $_SERVER['QUERY_STRING'], true);
+die();
--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -22,7 +22,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/formValidation.min.css">
+    <link rel="stylesheet" href="/css/formValidation.min.css">
 </head>
 
 <body>
