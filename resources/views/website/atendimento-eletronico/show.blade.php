2f947bcdf38003cbe2b9e87010160ea343d6e1f5
LEANDRO CAPARROZ <leandrocaparroz@yahoo.com.br>
2019-10-15 13:41:11 -0300

    Acerto de quebra de linha do atendimento eletrônico

--- resources/views/website/atendimento-eletronico/show.blade.php
+++ resources/views/website/atendimento-eletronico/show.blade.php
@@ -11,7 +11,7 @@
             <div class="mb-5">
                 <div class="bg-info rounded p-2">
                     <p>Texto enviado em  <span class="font-weight-bold">{{ dataHoraFormatted($chamado->dt_cadastro) }}</span></p>
-                    <p>{{ $chamado->ds_texto }}</p>
+                    <p>{!! nl2br($chamado->ds_texto) !!}</p>
                 </div>
 
                 <div class="bg-light rounded p-2 mt-4">
