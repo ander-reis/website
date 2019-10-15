1ca390232fd981c566ed38429286bd18bd9c829e
LEANDRO CAPARROZ <leandrocaparroz@yahoo.com.br>
2019-10-15 13:40:15 -0300

    Acerto de Quebra de  linha no atendimento eletrônico

--- resources/views/website/atendimento-eletronico/show.blade.php
+++ resources/views/website/atendimento-eletronico/show.blade.php
@@ -17,7 +17,7 @@
                 <div class="bg-light rounded p-2 mt-4">
                 @if($chamado->dt_resposta != NULL)
                     <p>Respondido pelo <span class="text-warning font-weight-bold">SinproSP</span> em  <span class="font-weight-bold">{{ dataHoraFormatted($chamado->dt_resposta) }}</span></p>
-                    <p>{{ $chamado->ds_texto_resposta }}</p>
+                    <p>{!! nl2br($chamado->ds_texto_resposta) !!}</p>
                 @else
                     <p>O SinproSP ainda não cadastrou uma resposta! Por favor, aguarde mais um pouco.</p>
                 @endif
