00afde6ccb2da1cfe8d2697320cde32f22fa3dd7
Ronaldo Araujo <ronaldog.araujo@gmail.com>
2020-02-03 15:57:26 -0300

    corrigido mensagem cabecalho do processo adicionado campos no cadastrar CPF em tb_sinpro_processos_professores

--- resources/views/website/processos/_message.blade.php
+++ resources/views/website/processos/_message.blade.php
@@ -1,6 +1,21 @@
 <div class="alert alert-danger text-center" role="alert">
     <div class="font-weight-bolder">Atenção!</div>
     <div>
-        {{ str_replace('@VR_RECEBER', moneyFormatted($message->first()->jur_prs_vr_receber) . " (" . moneyTextFormatted($message->first()->jur_prs_vr_receber) . ")", $message->first()->jur_psi_ds_status) }}
+
+       {!! str_replace(
+                '@VR_RECEBER',
+                moneyFormatted($message->first()->jur_prs_vr_receber) . " (" . moneyTextFormatted($message->first()->jur_prs_vr_receber) . ")",
+                str_replace(
+                    '@VR_PARCELA1',
+                    moneyFormatted($message->first()->jur_prs_vr_parcela1),
+                    str_replace(
+                        '@VR_PARCELA2',
+                        moneyFormatted($message->first()->jur_prs_vr_parcela2),
+                        $message->first()->jur_psi_ds_status
+                    )
+                )
+            )
+        !!}
+
     </div>
 </div>
