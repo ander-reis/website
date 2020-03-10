2c71e649bb0425e869126a8db72ebc1431e1af11
Anderson <ander-reis@hotmail.com>
2020-03-10 10:06:13 -0300

    Conclusão Calculor Reajuste atualização css

--- resources/views/website/calculo-reajuste/_form.blade.php
+++ resources/views/website/calculo-reajuste/_form.blade.php
@@ -9,7 +9,7 @@
         <span class="text-danger font-weight-bold">*</span>
         {{ Form::text('ds_fantasia', null, ['class' => 'form-control', 'disabled']) }}
     @endcomponent
-    @component('website.form-components._form_group_inline',['field' => 'fl_status', 'class' => 'col-md-6'])
+    @component('website.form-components._form_group_inline', ['field' => 'fl_status', 'class' => 'col-md-6 calculo-reajuste-radio'])
         <div class="radio">
             <div class="custom-control custom-radio custom-control-inline">
                 {{ Form::radio('fl_tipo', '0', true, ['class' => 'custom-control-input', 'id' => 'fl_tipo_ativo']) }}
