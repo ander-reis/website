ae8947ef0aec7cf95ae5d5e392cce13939132938
Ronaldo Araujo <ronaldog.araujo@gmail.com>
2020-03-24 12:39:52 -0300

    mensagem atendimento online

--- resources/views/website/emails/atendimento-eletronico.blade.php
+++ resources/views/website/emails/atendimento-eletronico.blade.php
@@ -1,7 +1,9 @@
 @component('mail::message')
 #Novo chamado cadastrado!
 
-O chamado de número <strong>{{ $id }}</strong> foi cadastrado em nosso website e deve ser respondido o mais breve possível.
+O chamado de número <strong>{{ $id }}</strong> foi cadastrado em nosso website.
+
+Devido ao alto número de atendimentos a distância, pode ser que sua resposta demore um pouco mais a chegar, porém iremos responder a todos. Você também pode falar conosco no número (11) 5080-5988, das 11h às 15h.
 
 @component('mail::button', ['url' => 'http://www1.sinprosp.org.br/admin'])
 Acesse aqui a Administração
