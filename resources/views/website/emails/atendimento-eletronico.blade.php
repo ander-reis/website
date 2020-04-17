ae60fca22e84db33e2f9ffc062d5c62b44e10206
Ronaldo Araujo <ronaldog.araujo@gmail.com>
2020-04-17 15:12:06 -0300

    msg feriado Tiradentes

--- resources/views/website/emails/atendimento-eletronico.blade.php
+++ resources/views/website/emails/atendimento-eletronico.blade.php
@@ -3,8 +3,6 @@
 
 O chamado de número <strong>{{ $id }}</strong> foi cadastrado em nosso website.
 
-Devido ao alto número de atendimentos a distância, pode ser que sua resposta demore um pouco mais a chegar, porém iremos responder a todos. Você também pode falar conosco no número (11) 5080-5988, das 11h às 15h.
-
 @component('mail::button', ['url' => 'http://www1.sinprosp.org.br/admin'])
 Acesse aqui a Administração
 @endcomponent
