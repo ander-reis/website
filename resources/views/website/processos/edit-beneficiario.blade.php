159cc7ce2ffb9066ab3a2d7cf959cce95ff8d4d0
Ronaldo Araujo <ronaldog.araujo@gmail.com>
2020-06-09 13:26:30 -0300

    posicao botao dados atualizados - Processos

--- resources/views/website/processos/edit-beneficiario.blade.php
+++ resources/views/website/processos/edit-beneficiario.blade.php
@@ -17,9 +17,15 @@
 
         @component('website.processos._form_beneficiario', ['model' => $model ?? null, 'cpf' => $cpf ?? null, 'id_processo' => $id_processo ?? null])@endcomponent
 
-        {{ Form::submit('Salvar', ['class' => 'btn btn-primary']) }}
+        <div class="row">
+            @component('website.form-components._form_col_group',['class' => 'col-12 col-sm-6 col-md-2 col-lg-2'])
+                {{ Form::submit('Salvar', ['class' => 'btn btn-primary']) }}
+            @endcomponent
 
-        {{ Form::submit('Dados atualizados', ['class' => 'btn btn-info']) }}
+            @component('website.form-components._form_col_group',['class' => 'col-12 col-sm-6 col-md-2 col-lg-2'])
+                {{ Form::submit('Meus dados já estão atualizados', ['class' => 'btn btn-info']) }}
+            @endcomponent
+        </div>
 
         {{ Form::close() }}
 
